df837c79486c3bc383de435dc45e46781cc498dd
Sergey <Dgoli.region37@yandex.ru>
2022-12-21 22:56:36 +0300

    Example_010

---
+++ Example_010/Program.cs
@@ -0,0 +1,17 @@
+﻿ Console.Clear();
+ int[] array = { 1, 15, 24, 78, 17, 94 };
+
+ int n = array.Length;
+ int find = 2;
+
+ int index = 0;
+
+ while (index < n)
+ {
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+ }
